57804b18cd6f932ad73061eee79c5e40b4636ab7
JuanTexeira <61384220+JuanTexeira@users.noreply.github.com>
2023-05-19 17:41:18 -0300

    Update localDeRopa-JuanTexeira.sql

--- localDeRopa-JuanTexeira.sql
+++ localDeRopa-JuanTexeira.sql
@@ -1,15 +1,6 @@
 CREATE SCHEMA localDeRopaTexeira;
 USE localDeRopaTexeira;
 
-CREATE TABLE prenda(
-	id_prenda INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre_prenda VARCHAR (50) NOT NULL,
-    stock INT NOT NULL,
-    precio NUMERIC (10,2) NOT NULL,
-    id_marca INT NOT NULL,
-    FOREIGN KEY (id_marca) REFERENCES marca(id_marca)
-);
-
 CREATE TABLE marca(
 	id_marca INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre_marca VARCHAR (50) NOT NULL,
@@ -17,14 +8,13 @@ CREATE TABLE marca(
     descuento INT NOT NULL
 );
 
-CREATE TABLE venta(
-	id_venta INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_vendedor INT NOT NULL,
-    FOREIGN KEY (id_vendedor) REFERENCES vendedor(id_vendedor),
-    id_cliente INT NOT NULL,
-    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
-    fecha_venta DATE NOT NULL DEFAULT (CURRENT_DATE),
-    monto NUMERIC (10,2) NOT NULL
+CREATE TABLE prenda(
+	id_prenda INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre_prenda VARCHAR (50) NOT NULL,
+    stock INT NOT NULL,
+    precio NUMERIC (10,2) NOT NULL,
+    id_marca INT NOT NULL,
+    FOREIGN KEY (id_marca) REFERENCES marca(id_marca)
 );
 
 CREATE TABLE vendedor(
@@ -41,6 +31,16 @@ CREATE TABLE cliente(
     fecha_nacimiento DATE NOT NULL,
     domicilio_cliente VARCHAR (50) NOT NULL,
     telefono VARCHAR (50) NOT NULL
+);
+
+CREATE TABLE venta(
+	id_venta INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_vendedor INT NOT NULL,
+    FOREIGN KEY (id_vendedor) REFERENCES vendedor(id_vendedor),
+    id_cliente INT NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    fecha_venta DATE NOT NULL DEFAULT (CURRENT_DATE),
+    monto NUMERIC (10,2) NOT NULL
 );
 
 CREATE TABLE detalle_venta(
